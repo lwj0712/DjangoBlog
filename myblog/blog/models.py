b653e08e94e34b4c96c3bce043afc29d7c15ef93
lwj0712 <dldnjswot@naver.com>
2024-08-24 21:32:59 +0900

    ✨ Update Category model

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
+    title = models.CharField(max_length=100)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
     content = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
