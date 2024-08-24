d43b985744c389bdeaaf1b8cfb64b839578c28ba
lwj0712 <dldnjswot@naver.com>
2024-08-24 20:34:19 +0900

    🔧 Adjust files DIR

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
-
+from django.views.generic import (
+    TemplateView,
+    ListView,
+    DetailView
+)
 class MainPageView(TemplateView):
     template_name = 'main_page.html'
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
