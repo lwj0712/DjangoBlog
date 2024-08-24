55ca5f9aba093e49e5c1ad32e7a7b0e14d05cbd7
lwj0712 <dldnjswot@naver.com>
2024-08-24 19:57:10 +0900

    ✨ Update Comment_model

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -12,7 +12,14 @@ class Post(models.Model):
         return self.title
 
 class Comment(models.Model):
-    pass
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    parent = models.ForeignKey('self', null=True, blank=True, related_name='replies', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Comment by {self.author} on {self.post}'
 
 class Like(models.Model):
     pass
