fda8e4a8182611e77fa209490c21d50f12ab5e31
lwj0712 <dldnjswot@naver.com>
2024-08-24 20:12:31 +0900

    Update config>urls.py

--- myblog/config/urls.py
+++ myblog/config/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from blog.views import MainPageView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', MainPageView.as_view(), name='main_page'),
     path('blog/', include('blog.urls')),
     path('accounts/', include('accounts.urls')),
 ]
