be0036300d4fff64d179bd32e22009597750276b
lwj0712 <dldnjswot@naver.com>
2024-08-24 20:45:03 +0900

    🐛 Bugfix : Mainpage url

--- myblog/config/urls.py
+++ myblog/config/urls.py
@@ -5,6 +5,6 @@ from blog.views import MainPageView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', MainPageView.as_view(), name='main_page'),
-    # path('blog/', include('blog.urls')),
+    path('blog/', include('blog.urls')),
     # path('accounts/', include('accounts.urls')),
 ]
