45beb4646729a26a71df3509dc02028f6310cc3b
lwj0712 <dldnjswot@naver.com>
2024-08-24 21:34:23 +0900

    🔧 Migrate Category model

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,12 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
 from .models import Post
+from .forms import PostForm
+from django.urls import reverse, reverse_lazy
 from django.views.generic import (
     TemplateView,
     ListView,
-    DetailView
+    DetailView,
+    UpdateView, 
+    DeleteView,
 )
+
+
 class MainPageView(TemplateView):
     template_name = 'main_page.html'
+
 
 
 class PostListView(ListView):
@@ -14,8 +22,53 @@ class PostListView(ListView):
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
 
+
+
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
     context_object_name = 'post'
     pk_url_kwarg = 'id'
+
+
+class PostCreateView(View):
+    def get(self, request):
+        form = PostForm()
+        return render(request, 'blog/post_form.html', {'form': form})
+
+    def post(self, request):
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('post_list')
+        return render(request, 'blog/post_form.html', {'form': form})
+    
+
+class PostUpdateView(UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+    pk_url_kwarg = 'id'
+    
+    def get_success_url(self):
+        return reverse('post_detail', kwargs={'id': self.object.id})
+    
+from django.urls import reverse, reverse_lazy
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    context_object_name = 'post'
+    pk_url_kwarg = 'id'
+    success_url = reverse_lazy('post_list')
+
+
+class PostSearchView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    
+    def get_queryset(self):
+        tag = self.kwargs['tag']
+        return Post.objects.filter(category__name__icontains=tag).order_by('-created_at')
