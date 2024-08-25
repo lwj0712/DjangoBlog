ce78a44d0fc9899caa6e162ff1c04796d196a845
lwj0712 <dldnjswot@naver.com>
2024-08-25 20:31:18 +0900

    ✨ Update blog>urls.py

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
-from .views import PostListView, PostDetailView
+from .views import (
+    PostListView, 
+    PostDetailView, 
+    PostCreateView, 
+    PostUpdateView, 
+    PostDeleteView, 
+    PostSearchView
+                    )
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post_list'),
     path('<int:id>/', PostDetailView.as_view(), name='post_detail'),
+    path('write/', PostCreateView.as_view(), name='post_create'),
+    path('edit/<int:id>/', PostUpdateView.as_view(), name='post_update'),
+    path('delete/<int:id>/', PostDeleteView.as_view(), name='post_delete'),
+    path('search/<str:tag>/', PostSearchView.as_view(), name='post_search'),
 ]
