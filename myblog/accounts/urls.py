43515ed987d6e012e6f78aca13c142b199ba6b64
lwj0712 <dldnjswot@naver.com>
2024-08-28 14:00:24 +0900

    ✨ Update ProfileUpdate

--- myblog/accounts/urls.py
+++ myblog/accounts/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import SignUpView
+from .views import SignUpView, UserProfileUpdateView
 
 app_name = 'accounts'
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('login/', LoginView.as_view(redirect_authenticated_user=True), name='login'),
     path('logout/', LogoutView.as_view(next_page='main_page'), name='logout'),
     path('register/', SignUpView.as_view(), name='register'),
+    path('edit/', UserProfileUpdateView.as_view(), name='edit'),
 ]
