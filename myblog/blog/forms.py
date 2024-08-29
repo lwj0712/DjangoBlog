8f83e6f2952615826188fc8e866c39171f09609b
lwj0712 <dldnjswot@naver.com>
2024-08-29 15:25:56 +0900

    ✨ Update forms for CommentReply

--- myblog/blog/forms.py
+++ myblog/blog/forms.py
@@ -11,3 +11,13 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['content']
+
+
+class CommentReplyForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+
+    def __init__(self, *args, **kwargs):
+        self.parent_comment_id = kwargs.pop('parent_comment_id', None)
+        super().__init__(*args, **kwargs)
