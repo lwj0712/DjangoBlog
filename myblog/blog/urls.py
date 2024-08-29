6aef2767404169032d361b6c328506671450b748
lwj0712 <dldnjswot@naver.com>
2024-08-29 15:31:56 +0900

    🔧 Update urls : comment add, comment delete, comment reply & Fix every id -> pk

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -5,10 +5,12 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
-    path('<int:id>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
     path('write/', views.PostCreateView.as_view(), name='post_create'),
-    path('edit/<int:id>/', views.PostUpdateView.as_view(), name='post_update'),
-    path('delete/<int:id>/', views.PostDeleteView.as_view(), name='post_delete'),
+    path('edit/<int:pk>/', views.PostUpdateView.as_view(), name='post_update'),
+    path('delete/<int:pk>/', views.PostDeleteView.as_view(), name='post_delete'),
     path('search/<str:tag>/', views.PostSearchView.as_view(), name='post_search'),
-    
+    path('comment/<int:post_pk>/add/', views.CommentCreateView.as_view(), name='comment_create'),
+    path('comment/<int:comment_pk>/delete/', views.CommentDeleteView.as_view(), name='comment_delete'),
+    path('comment/<int:post_pk>/<int:comment_pk>/reply/', views.CommentReplyView.as_view(), name='comment_reply'),
 ]
