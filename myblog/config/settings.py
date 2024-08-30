25ad5c1dd2c6e86a786bed523f2e258dd2dd05fc
lwj0712 <dldnjswot@naver.com>
2024-08-30 19:12:52 +0900

    🔧 Add crispy module

--- myblog/config/settings.py
+++ myblog/config/settings.py
@@ -27,7 +27,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     'blog',
+    "crispy_forms",
+    "crispy_bootstrap4",
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
